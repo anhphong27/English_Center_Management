8099edf080d98efff85e349d553d388cce46ac45
Phong <phonglna27@gmail.com>
2026-03-18 23:19:49 +0700

    configure app and CORS

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # libraries
+    'rest_framework',
+    'corsheaders',
+    # apps
+    'api'
+
 ]
 
 MIDDLEWARE = [
@@ -47,6 +53,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
 ]
 
 ROOT_URLCONF = 'core.urls'
