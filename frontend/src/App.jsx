1bd4e2203845f526e220658601d020d71633c1bf
Phong <phonglna27@gmail.com>
2026-03-19 16:42:04 +0700

    setup frontend with React, Vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { CssBaseline } from '@mui/material';
+
+function App() {
+  return (
+    <>
+      <CssBaseline /> {/* Reset CSS mặc định của trình duyệt để hợp với MUI */}
+      <div>
+        <h1>Hệ thống Quản lý Trung tâm Tiếng Anh (ECMS)</h1>
+        <p>Frontend đã sẵn sàng hoạt động!</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
